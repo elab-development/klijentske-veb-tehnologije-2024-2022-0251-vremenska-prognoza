81282fcba8b5e72736744c5adb0e67755ddac4f2
Savic23 <aleksandarsavic23062003@gmail.com>
2024-06-20 23:22:45 +0200

    not-found stranica

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeToggler/theme-provider";
 import Layout from "./layout/Layout";
 import Home from "./pages/home/Home";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <ThemeProvider defaultTheme="dark" storageKey="theme">
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+            <Route index element={<Home />} />{" "}
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </ThemeProvider>
