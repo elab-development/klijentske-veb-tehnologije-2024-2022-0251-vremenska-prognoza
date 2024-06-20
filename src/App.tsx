3a576b0a704257e6e66a9a44768b1dcbd4ff8059
RadeStojicic <radestojicicsd@gmail.com>
2024-06-20 20:23:00 +0200

    responsive navbar + themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ThemeProvider } from "./components/ThemeToggler/theme-provider";
 import Layout from "./layout/Layout";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
+      <ThemeProvider defaultTheme="dark" storageKey="theme">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </Router>
   );
 }
--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -1,18 +1,36 @@
+import { ThemeToggler } from "@/components/ThemeToggler/Toggler";
+import { useTheme } from "@/components/ThemeToggler/theme-provider";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Cross2Icon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import ResponsiveNavbar from "./ResponsiveNavbar";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const links = [
     { href: "/", name: "Home" },
     { href: "/news", name: "News" },
     { href: "/contact", name: "Contact" },
     { href: "/log-in", name: "Log in" },
   ];
+  const theme = useTheme().theme;
   return (
     <nav className="fixed top-0 z-[1] mx-auto flex h-20 w-full items-center justify-between border-b border-foreground/10 bg-background/70 backdrop-blur-lg">
       <div className="container mx-auto flex w-full justify-between">
-        <Link className="flex items-center" to="/">
-          <img src="/logo/weather-logo-black.svg" alt="Logo" />
+        <Link
+          onClick={() => setIsOpen(false)}
+          className="flex items-center"
+          to="/"
+        >
+          <img
+            src={cn({
+              "/logo/weather-logo-white.svg": theme === "dark",
+              "/logo/weather-logo-black.svg": theme !== "dark",
+            })}
+            alt="Logo"
+          />
         </Link>
         <ul className="hidden items-center justify-end gap-8 md:flex">
           {links.map(({ href, name }) =>
@@ -28,7 +46,30 @@ const Navbar = () => {
               </li>
             ),
           )}
+          <ThemeToggler />
         </ul>
+        <div className="md:hidden">
+          <div className="flex items-center gap-2">
+            <ThemeToggler />
+            <Button
+              className="px-0 hover:bg-transparent"
+              size="icon"
+              variant="ghost"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {isOpen ? (
+                <Cross2Icon width={32} height={32} />
+              ) : (
+                <HamburgerMenuIcon width={32} height={32} />
+              )}
+            </Button>
+          </div>
+          <ResponsiveNavbar
+            links={links}
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+          />
+        </div>
       </div>
     </nav>
   );
