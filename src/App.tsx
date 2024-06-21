a11c196cf16a65e16af260fc4176ff29a45b4838
suzanatesovic03 <suzanatesovic03@gmail.com>
2024-06-21 10:58:54 +0200

    dodata kontakt stranica bez forme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeToggler/theme-provider";
 import { ForecastProvider } from "./hooks/useForecast";
 import Layout from "./layout/Layout";
+import Contact from "./pages/contact/Contact";
 import Home from "./pages/home/Home";
 import NotFound from "./pages/notFound/NotFound";
 
@@ -12,7 +13,8 @@ function App() {
         <ThemeProvider defaultTheme="dark" storageKey="theme">
           <Routes>
             <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />{" "}
+              <Route index element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
               <Route path="*" element={<NotFound />} />
             </Route>
           </Routes>
