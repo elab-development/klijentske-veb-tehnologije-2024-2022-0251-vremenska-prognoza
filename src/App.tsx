2ab9dd469ff4c0339ef15358d5d96dc28d765cec
RadeStojicic <radestojicicsd@gmail.com>
2024-06-21 18:10:22 +0200

    added login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeToggler/theme-provider";
 import { ForecastProvider } from "./hooks/useForecast";
 import Layout from "./layout/Layout";
+import LogIn from "./pages/auth/LogIn";
 import Contact from "./pages/contact/Contact";
 import Home from "./pages/home/Home";
 import NotFound from "./pages/notFound/NotFound";
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
               <Route path="/contact" element={<Contact />} />
+              <Route path="/log-in" element={<LogIn />} />
               <Route path="*" element={<NotFound />} />
             </Route>
           </Routes>
